1cbcc508805e4e4cedd2f4eca52759e7f333b160
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 13:28:25 +0330

    Add ProductDto

---
+++ src/Application/DTOs/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string CreatedByUserId { get; set; }
+    public string Name { get; set; }
+    public DateOnly ProduceDate { get; set; }
+    public string ManufactureEmail { get; set; }
+    public string ManufacturePhone { get; set; }
+    public bool IsAvailable { get; set; }
+}
