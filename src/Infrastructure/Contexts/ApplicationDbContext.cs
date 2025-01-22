52d58dc06c777874c09b42f8406b88a0471a4d7c
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 23:06:06 +0330

    Inherit app DbContext from IdentityDbContext

--- src/Infrastructure/Contexts/ApplicationDbContext.cs
+++ src/Infrastructure/Contexts/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
 
 namespace Infrastructure.Persistence.Contexts;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
