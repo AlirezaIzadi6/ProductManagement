ca07dc0bf1699246a913e1b17e4ef639f1cb98be
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 10:28:24 +0330

    Create repository interfaces

---
+++ src/Application/Interfaces/IGenericRepositoryAsync.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Interfaces;
+
+public interface IGenericRepositoryAsync<T> where T : class
+{
+    Task<T> CreateAsync(T entity);
+    Task<T?> GetByIdAsync<TId>(TId id);
+    Task<IEnumerable<T>> ListAsync();
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ src/Application/Interfaces/IProductRepositoryAsync.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
+{
+    Task<IEnumerable<Product>> GetProductsByCreatorAsync(string CreatorId);
+}
