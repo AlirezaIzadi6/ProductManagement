a21aa5b6b328a467e5c1270d4eb676921e0caddb
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 22:57:37 +0330

    Create generic and product repositories

---
+++ src/Infrastructure/Repositories/GenericRepositoryAsync.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : BaseEntity
+{
+    private readonly ApplicationDbContext _context;
+    public GenericRepositoryAsync(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> ListAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Set<T>().Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ src/Infrastructure/Repositories/ProductRepositoryAsync.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Application.Interfaces;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ProductRepositoryAsync : GenericRepositoryAsync<Product>, IProductRepositoryAsync
+{
+    private readonly ApplicationDbContext _context;
+    public ProductRepositoryAsync(ApplicationDbContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product?> GetByIdAsync(int id)
+    {
+        return await _context.products.FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsByCreatorAsync(string creatorId)
+    {
+        return await _context.products.Where(p => p.CreatedByUserId == creatorId)
+            .ToListAsync();
+    }
+
+    public async Task<bool> IsUnique(string name, DateOnly produceDate)
+    {
+        return await _context.products.AnyAsync(p => p.Name == name && p.ProduceDate == produceDate);
+    }
+}
