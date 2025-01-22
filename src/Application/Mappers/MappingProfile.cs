c8d93cdad69e54bd14a77ae32e82195a771e9660
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 13:27:42 +0330

    Create AutoMapper profile

---
+++ src/Application/Mappers/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.DTOs;
+
+namespace Application.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+}
