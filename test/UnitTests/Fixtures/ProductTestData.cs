5c54dc174e184719eaf363056b4ee9312e33230e
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 13:29:12 +0330

    Create test data for product

---
+++ test/UnitTests/Fixtures/ProductTestData.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Application.DTOs;
+
+namespace UnitTests.Fixtures;
+
+public static class ProductTestData
+{
+    public static readonly Product ProductSample = new Product
+    {
+        Id = 1,
+        CreatedByUserId = new Guid().ToString(),
+        Name = "Test",
+        ProduceDate = DateOnly.FromDateTime(DateTime.Now),
+        ManufactureEmail = "test@test.com",
+        ManufacturePhone = "1234567890",
+        IsAvailable = true,
+    };
+
+    public static readonly ProductDto ProductDtoSample = new ProductDto
+    {
+        Id = 1,
+        CreatedByUserId = new Guid().ToString(),
+        Name = "Test",
+        ProduceDate = DateOnly.FromDateTime(DateTime.Now),
+        ManufactureEmail = "test@test.com",
+        ManufacturePhone = "1234567890",
+        IsAvailable = true,
+    };
+}
