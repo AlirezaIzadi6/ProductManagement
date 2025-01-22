1e5cc710ad6016cacaf9bfa3962b1f8d48acabeb
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 22:55:41 +0330

    Create db context

---
+++ src/Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> products { get; set; }
+}
