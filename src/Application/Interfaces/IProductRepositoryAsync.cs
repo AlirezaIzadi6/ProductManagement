c39772ecbe5e9f02c1cb79cc4d209c98cb37c912
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 20:15:30 +0330

    Add a method to check product uniqueness

--- src/Application/Interfaces/IProductRepositoryAsync.cs
+++ src/Application/Interfaces/IProductRepositoryAsync.cs
@@ -5,4 +5,5 @@ namespace Application.Interfaces;
 public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
 {
     Task<IEnumerable<Product>> GetProductsByCreatorAsync(string CreatorId);
+    Task<bool> IsUnique(string name, DateOnly produceDate);
 }
