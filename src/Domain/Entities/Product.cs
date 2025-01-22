3bd5aeb5957acdc82d40e044d4d9297461f0f17c
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 08:46:29 +0330

    Setup domain project

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public DateOnly ProduceDate { get; set; }
+    public string ManufactureEmail { get; set; }
+    public string ManufacturePhone { get; set; }
+    public bool IsAvailable { get; set; }
+}
