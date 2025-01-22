67b51e5a43321cbb7ffdffcf108f8549ccbf959c
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 21:17:18 +0330

    Add delete product functionality

---
+++ src/Application/Features/Products/Delete/DeleteProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Application.Wrappers;
+
+namespace Application.Features.Products.Delete;
+
+public class DeleteProductCommand : IRequest<Response<int>>
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+}
---
+++ src/Application/Features/Products/Delete/DeleteProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Application.Interfaces;
+using Application.Wrappers;
+
+namespace Application.Features.Products.Delete;
+
+public class DeleteProductHandler : BaseProductCommandHandler, IRequestHandler<DeleteProductCommand, Response<int>>
+{
+    public DeleteProductHandler(IProductRepositoryAsync repository, IMapper mapper)
+        : base(repository, mapper)
+    {
+    }
+
+    public async Task<Response<int>> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = await _repository.GetByIdAsync(command.Id);
+        if (product == null) return new Response<int>("Product not found");
+
+        if (command.UserId != product.CreatedByUserId) return new Response<int>("Not authorized");
+
+        await _repository.DeleteAsync(product);
+        return Response<int>.SuccessResponse;
+    }
+}
