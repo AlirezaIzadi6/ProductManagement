063b2b9320b4910a44a9e2b3648f6a557e7742f2
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 10:29:16 +0330

    Create response object for returning unified format response to outer layers

---
+++ src/Application/Wrappers/Response.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Wrappers;
+
+public class Response<T>
+{
+    public bool Succeded { get; set; }
+    public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
+    public T? Data { get; set; }
+
+    public Response(string message)
+    {
+        Succeded = false;
+        Message = message;
+    }
+
+    public Response(T data, string? message = null)
+    {
+        Succeded = true;
+        Data = data;
+        Message = message;
+    }
+}
---
+++ test/UnitTests/Response.test.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Application.Wrappers;
+
+namespace UnitTests;
+
+public class ResponseTest
+{
+    [Fact]
+    public void CreateResponseWithData_ReturnsData()
+    {
+        var product = new Product
+        {
+            Id = 1,
+            CreatedByUserId = new Guid().ToString(),
+            Name = "Test",
+            ProduceDate = DateOnly.FromDateTime(DateTime.Now),
+            ManufactureEmail = "test@example.com",
+            ManufacturePhone = "1234567890",
+            IsAvailable = true
+        };
+        var response = new Response<Product>(product);
+        Assert.Equal(true, response.Succeded);
+        Assert.NotNull(response.Data);
+        Assert.Equal(1, response.Data.Id);
+    }
+}
