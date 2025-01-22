3c88a336b6e7a785df1f3ab83f3cbe74c4db878d
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-22 22:57:17 +0330

    Move GetByIdAsync method to inheritted interfaces

--- src/Application/Interfaces/IGenericRepositoryAsync.cs
+++ src/Application/Interfaces/IGenericRepositoryAsync.cs
@@ -3,7 +3,6 @@
 public interface IGenericRepositoryAsync<T> where T : class
 {
     Task<T> CreateAsync(T entity);
-    Task<T?> GetByIdAsync<TId>(TId id);
     Task<IEnumerable<T>> ListAsync();
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
--- src/Application/Interfaces/IProductRepositoryAsync.cs
+++ src/Application/Interfaces/IProductRepositoryAsync.cs
@@ -4,6 +4,7 @@ namespace Application.Interfaces;
 
 public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
 {
+    Task<Product?> GetByIdAsync(int id);
     Task<IEnumerable<Product>> GetProductsByCreatorAsync(string CreatorId);
     Task<bool> IsUnique(string name, DateOnly produceDate);
 }
