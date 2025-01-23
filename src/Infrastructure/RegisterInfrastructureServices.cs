b2c0aace97e897e144b30e57b34f679c3a3bef9e
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-23 16:04:19 +0330

    Register infrastructure services to DI container

---
+++ src/Infrastructure/RegisterInfrastructureServices.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using Infrastructure.Services;
+using Infrastructure.Persistence.Contexts;
+using Infrastructure.Persistence.Repositories;
+
+namespace Infrastructure;
+
+public static class RegisterInfrastructureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("MsSqlConnection"),
+                b => b.MigrationsAssembly("Infrastructure"));
+        });
+
+        services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<IProductRepositoryAsync, ProductRepositoryAsync>();
+        // Just a fake service for Identity package to work:
+        services.AddTransient<IEmailSender<IdentityUser>, EmailSender>();
+
+        return services;
+    }
+}
