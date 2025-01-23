894432441a88c85f2107ecd99a7154859ae98186
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-23 16:15:17 +0330

    - Create migrations. - Create fake email sender service. - Add Program.cs file.

---
+++ src/Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,22 @@
+﻿// This is just a fake service and does not do anything. The purpose of this service is to make Identity services work.
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Services;
+
+public class EmailSender : IEmailSender<IdentityUser>
+{
+#pragma warning disable CS1998
+    public async Task SendConfirmationLinkAsync(IdentityUser user, string s1, string s2) { }
+
+    public async Task SendPasswordResetLinkAsync(IdentityUser user, string s1, string s2) { }
+
+    public async Task SendEmailAsync(string s1, string s2, string s3) { }
+
+    public async Task SendPasswordResetCodeAsync(IdentityUser user, string s1, string s2) { }
+#pragma warning restore CS1998
+}
---
+++ src/Web/Program.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using AutoMapper;
+using Application;
+using Infrastructure;
+using Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddCookie("Identity.Bearer");
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapIdentityApi<IdentityUser>();
+app.Run();
