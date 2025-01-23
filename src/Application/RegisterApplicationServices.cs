154580981b3155ccbcfe0ec1362999100bcc2c70
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-23 15:47:04 +0330

    Register application services in dependency injection container

---
+++ src/Application/RegisterApplicationServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using Application.Mappers;
+using System.Reflection;
+
+namespace Application;
+
+public static class RegisterApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(conf => conf.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
