dedd72126bdf47447600bf41115a73f481aeb855
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-24 18:05:50 +0330

    Add custom WebApplicationBuilder

---
+++ test/FunctionalTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FunctionalTests;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("FunctionalTestsDatabase");
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var dbContext = scopedServices.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+        });
+    }
+
+    public void CustomConfigureServices(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            }
+        })
+    }
+}
