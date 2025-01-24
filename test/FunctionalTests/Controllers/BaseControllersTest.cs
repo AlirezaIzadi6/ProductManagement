586de1f5ddb1f0cec33c1543b55443f0bc52506f
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-24 18:13:28 +0330

    Add base controllers test class

---
+++ test/FunctionalTests/Controllers/BaseControllersTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace FunctionalTests.Controllers;
+
+public class BaseControllersTest : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public BaseControllersTest(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    public HttpClient GetNewClient()
+    {
+        var newClient = _factory.WithWebHostBuilder(builder =>
+        {
+            _factory.CustomConfigureServices(builder);
+        });
+
+        return newClient;
+    }
+}
